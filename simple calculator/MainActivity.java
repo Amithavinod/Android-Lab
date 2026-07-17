86e4ede7d8e8d30d6243e3646cbcce67bc6e7574
Amithavinod <amithavinod2005@gmail.com>
2026-07-17 11:08:16 +0530

    Implement basic arithmetic operations in MainActivity

---
+++ simple calculator/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.simplecalculator;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText n1,n2;
+
+    Button sum,sub,mul,div;
+
+    TextView rslt;
+
+    @SuppressLint("MissingInflatedId")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        n1 = (EditText) findViewById(R.id.editTextText);
+        n2 = (EditText) findViewById(R.id.editTextText2);
+        sum = findViewById(R.id.button4);
+        sub = findViewById(R.id.button5);
+        mul = findViewById(R.id.button6);
+        div = findViewById(R.id.button8);
+        rslt = findViewById(R.id.textView);
+
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f1 = Integer.parseInt(n1.getText().toString());
+                int f2 = Integer.parseInt(n2.getText().toString());
+                int cal = f1 + f2;
+
+                rslt.setText("Addition = "+cal);
+
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f1 = Integer.parseInt(n1.getText().toString());
+                int f2 = Integer.parseInt(n2.getText().toString());
+                int cal = f1 - f2;
+
+                rslt.setText("Subtraction = "+cal);
+
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f1 = Integer.parseInt(n1.getText().toString());
+                int f2 = Integer.parseInt(n2.getText().toString());
+                int cal = f1 * f2;
+
+                rslt.setText("Multiplication = "+cal);
+
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f1 = Integer.parseInt(n1.getText().toString());
+                int f2 = Integer.parseInt(n2.getText().toString());
+                int cal = f1 / f2;
+
+                rslt.setText("Division = "+cal);
+
+            }
+        });
+
+
+
+    }
+}
